fad6aaeb14391d49504120174b7d08aa0f1e0dab
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-18 09:29:31 -0600

    Update email.py

--- email.py
+++ email.py
@@ -1,25 +1,32 @@
 import smtplib
 from smtp_secret import password
 passwd = password
-emails = ["list","of","addressess"]
+# Set up email details
+sender_email = "send as address"
+sender_display_name = "Display name" # replace with your desired display name
+reply_to_email = "<reply to address"
+subject = "Example TEST"
+emails = ["usera@example.com","userb@example.com"]
 for email in emails:
-    sender_email = "<replace with sender_email address"
-    sender_display_name = "<Desired Display Name>"
-    receiver_email = email
-    reply_to_email = "<replace with reply-to email address>"
-    message = f"""Subject: Example
-    From: {sender_display_name} <{sender_email}>
-    Reply-To: {reply_to_email}
-      
-    Body text to {email} goes here. 
-    """
-    message = message.encode('utf8')
-    # connect to the SMTP server and send the email
-    # be sure to replace with your smtp server
-    with smtplib.SMTP("smtp.office365.com", 587) as server:
-        # log in to the email account
-        server.starttls()
-        server.login(sender_email, passwd)
-        server.sendmail(sender_email, receiver_email, message)
+    body = f"Body text to {email} goes here."
+
+    message_parts = [
+        f"Subject: {subject}",
+        f"From: {sender_display_name} <{sender_email}>",
+        f"To: {email}",
+        f"Reply-To: {reply_to_email}",
+        "",
+        body,
+    ]
+    message = "\n".join(message_parts)
+
+    try:
+        with smtplib.SMTP("smtp.office365.com", 587) as server:
+            server.starttls()
+            server.login(sender_email, passwd)
+            server.sendmail(sender_email, email, message)
 
         print(f"Email sent to {email}")
+    except Exception as e:
+        print(f"Error sending email to {email}: {e}")
+
