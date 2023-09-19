a2ea4e0061abee8dc766dccea799a8cb1d1856b4
Alex Noel <116229513+anoel2@users.noreply.github.com>
2023-09-19 10:08:31 -0600

    Update email_append.ps1

--- email_append.ps1
+++ email_append.ps1
@@ -1,4 +1,5 @@
+#Use this powershell script if you need to append a domain name to a list of names. 
 $inputFile = names.txt # Replace with the name of your input file
 $outputFile = email.txt # Replace with the name you want for the modified file
 
-Get-Content $inputFile  ForEach-Object { $_ + @colorado.edu }  Set-Content $outputFile
+Get-Content $inputFile  ForEach-Object { $_ + @example.com }  Set-Content $outputFile
